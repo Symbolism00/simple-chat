a13c0edea10d5dfcbbf2cae02551c36a008ab0a7
Luis Ferreira <1180929@isep.ipp.pt>
2023-05-17 23:23:58 +0100

    Update chat to our GetCloser Backend

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -14,45 +14,100 @@ const Chat = () => {
   const [message, setMessage] = useState<string>("");
   const [messages, setMessages] = useState<MessageStruct[]>([]);
   const [socket, setSocket] = useState<Socket | undefined>(undefined);
+  const [isUserA, setIsUserA] = useState<boolean>(true);
+  const [chatId, setChatId] = useState<string>("");
+  const [offsetMessages, setOffsetMessages] = useState<number>(10);
 
   useEffect(() => {
     if (socket === undefined) {
-      setSocket(io("http://localhost:4000"));
+      setSocket(io("http://localhost:3001"));
     }
   }, [socket]);
 
   useEffect(() => {
-    socket?.on("message-server", (username, message, serverMessage) => {
-      setMessages([
-        ...messages,
+    socket?.on("joinedChat", (chat) => {
+      setChatId(chat.chatId);
+      setMessages(
+        chat.messages.map((message: any) => {
+          return {
+            username: message.userId,
+            message: message.content,
+            myMessage: true,
+            serverMessage: false,
+          };
+        })
+      );
+    });
+
+    socket?.on("newChatMessage", (message) => {
+      const newMessages: MessageStruct[] = [
         {
-          username: username,
+          username: isUserA ? "2" : "1",
           message: message,
-          serverMessage: serverMessage,
-          myMessage: false,
+          serverMessage: false,
+          myMessage: true,
         },
-      ]);
+      ];
+      setMessages(newMessages.concat(messages));
+      const offset: number = offsetMessages + 1;
+      setOffsetMessages(offset);
+    });
+
+    socket?.on("collectedChatHistory", (chat) => {
+      const oldMessages: MessageStruct[] = messages;
+      const chatHistory: MessageStruct[] = chat.messages.map((message: any) => {
+        return {
+          username: message.userId,
+          message: message.content,
+          myMessage: true,
+          serverMessage: false,
+        };
+      });
+      setMessages(oldMessages.concat(chatHistory));
     });
   }, [socket, messages]);
 
-  const joinChat = () => {
+  const joinChat = (isUserA: boolean) => {
     setUsernameChecked(true);
-    socket?.emit("join", 123);
-    socket?.emit("joined", username, 123);
+    setIsUserA(isUserA);
+    socket?.emit("joinChat", { userIdA: 1, userIdB: 2 });
   };
 
   const submitMessage = () => {
-    socket?.emit("message-client", username, message, 123);
-    setMessages([
-      ...messages,
+    socket?.emit("chatMessage", {
+      chatId: chatId,
+      userId: isUserA ? 1 : 2,
+      content: message,
+    });
+    const newMessages: MessageStruct[] = [
       {
-        username: username,
+        username: isUserA ? "1" : "2",
         message: message,
         serverMessage: false,
         myMessage: true,
       },
-    ]);
+    ];
+    setMessages(newMessages.concat(messages));
     setMessage("");
+    const offset: number = offsetMessages + 1;
+    setOffsetMessages(offset);
+  };
+
+  const loadChatHistory = (e: any) => {
+    if (
+      e.currentTarget.scrollHeight -
+        e.currentTarget.clientHeight +
+        e.currentTarget.scrollTop ===
+      0
+    ) {
+      socket?.emit("chatHistory", {
+        chatId: chatId,
+        offset: offsetMessages,
+        limit: 10,
+      });
+      const offset: number = offsetMessages + 10;
+      setOffsetMessages(offset);
+    }
   };
 
   return (
@@ -60,25 +115,27 @@ const Chat = () => {
       <div className="flex flex-row gap-6 mb-8">
         {!usernameChecked ? (
           <>
-            <input
-              className="flex flex-col bg-gray-200 w-48 m-1 p-1 border-2 border-black"
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            ></input>
             <button
-              className="border-2 border-black bg-purple-200 h-10 m-1 hover:bg-purple-300 w-24"
-              onClick={joinChat}
+              className="border-2 border-black bg-blue-200 h-10 m-1 hover:bg-blue-300 w-24"
+              onClick={() => joinChat(true)}
             >
-              Join
+              User A
+            </button>
+            <button
+              className="border-2 border-black bg-green-200 h-10 m-1 hover:bg-green-300 w-24"
+              onClick={() => joinChat(false)}
+            >
+              User B
             </button>
           </>
         ) : (
-          <h1 className="text-xl">Welcome {username}, let's start chatting!</h1>
+          <h1 className="text-xl">Welcome {username}, lets start chatting!</h1>
         )}
       </div>
-      <div className="bg-green-50 h-96 border-2 border-black w-96 overflow-auto">
+      <div
+        className="flex bg-green-50 h-96 border-2 border-black w-96 overflow-auto flex-col-reverse"
+        onScroll={loadChatHistory}
+      >
         {messages.map((messageStruct, index) => {
           return (
             <div key={`${index}`} className={`flex flex-row p-1`}>
